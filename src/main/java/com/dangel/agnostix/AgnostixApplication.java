36ce69a3c8a4f98375af40f88584b6a6055b8bad
Daniel <daniel.sipek@etnetera.cz>
2025-04-24 11:09:14 +0200

    Initial commit

---
+++ src/main/java/com/dangel/agnostix/AgnostixApplication.java
@@ -0,0 +1,13 @@
+package com.dangel.agnostix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgnostixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgnostixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dangel/agnostix/AgnostixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangel.agnostix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgnostixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
