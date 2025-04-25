6c9529b7a99d035304c95d37e6011e5ae64e6db7
Daniel <daniel.sipek@etnetera.cz>
2025-04-25 16:14:42 +0200

    implemented basic HealthCheck

---
+++ src/main/java/com/dangel/agnostix/rest/HealthCheck.java
@@ -0,0 +1,31 @@
+package com.dangel.agnostix.rest;
+
+
+import com.dangel.agnostix.connectors.CnbService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    CnbService cnbservice;
+
+    public HealthCheck(CnbService cnbservice) {
+        this.cnbservice = cnbservice;
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><head><title>Health Check</title></head><body>");
+        sb.append("<h1>WebService</h1>");
+        sb.append("State: OK");
+        sb.append("<h1>CNB</h1>");
+        sb.append("State: " ).append(cnbservice.getState().toString());
+        sb.append("<h1>WebService</h1>");
+        sb.append("State: OK");
+        sb.append("</body></html>");
+
+        return sb.toString();
+    }
+}
